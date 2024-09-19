575a5ee2d5767b2cd9eab3d8d1d598479b769416
ivan.martinez <ivan.martinez@quipux.com>
2024-09-19 17:55:33 -0500

    fix: arregla variables de entorno en mongo_setup.

--- mongo_setup.py
+++ mongo_setup.py
@@ -8,7 +8,6 @@ from pymongo.database import Database
 def create_mongo_client() -> MongoClient:
     """Crea y devuelve un cliente de MongoDB usando las variables de entorno."""
     # Usa un valor por defecto si no está definido
-
     mongo_host = os.environ.get('MONGODB_HOST', 'localhost')
     # Convierte el puerto a entero
     mongo_port = int(os.environ.get('MONGODB_PORT', 27017))
