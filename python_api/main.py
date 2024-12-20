1e7826ea022e9b14e08e00730d91cc60756599ea
ivan.martinez <ivan.martinez@quipux.com>
2024-12-20 10:32:19 -0500

    feat: Realiza cambios en importacion y archivos docker.

--- python_api/main.py
+++ python_api/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Query
 from datetime import datetime
 
 # librerias propias
-from python_api.mongo_setup import get_mongo_db
+from mongo_setup import get_mongo_db
 
 app = FastAPI()
 
