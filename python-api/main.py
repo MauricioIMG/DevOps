836fc8bd47baba608ea45e1e1f92b2c64ff70d12
ivan.martinez <ivan.martinez@quipux.com>
2024-12-20 23:26:11 -0500

    feat: Configuración del python-api.

--- python-api/main.py
+++ python-api/main.py
@@ -25,28 +25,23 @@ def save_non_ordered_list(lista_no_ordenada: str = Query(
         diccionario con mensaje con ID único de guardado de la lista.
 
     """
-    # Retorna la lista recibida por parámetro
     try:
         lista_no_ordenada = [int(x) for x in lista_no_ordenada.strip("[]").split(",")]
     except ValueError:
         return {"error": "La lista debe contener solo números separados por comas."}
-    # Retorna la hora actual formateada ("HHh:MM:SS")
+
     tiempo = datetime.now().strftime("%Hh:%M:%S")
-    # Obtiene un Id único para la lista procesada
+
     id_unico = str(uuid.uuid4())
 
-    # Diccionario con datos para insertar registro en mongo
     data = {
         "id": id_unico,
         "lista_no_ordenada": lista_no_ordenada,
         "hora_sistema": tiempo
     }
 
-    # Instancia para obtener base de datos "python_app"
     db = get_mongo_db()
-    # Coleccion dentro de mongodb para guardar registros
     coleccion = db.listas_no_ordenadas
-    # Inserta el registro dentro de la colección en mongodb
     coleccion.insert_one(data)
 
     respuesta = {"msg": f"La lista no ordenada fue guardada con el id: {id_unico}"}
@@ -69,11 +64,10 @@ def get_ordered_list(lista_no_ordenada: str = Query(
         diccionario con lista ordenada con hora actual del sistema.
 
     """
-    # Retorna ordenada la lista recibida por parámetro
     lista_ordenada = sorted([int(x) for x in lista_no_ordenada.strip("[]").split(",")])
-    # Retorna la hora actual formateada ("HHh:MM:SS")
+
     tiempo = datetime.now().strftime("%Hh:%M:%S")
-    # Crea un formato JSON para la respuesta del Endpoint
+
     respuesta = {
                 "hora_sistema": tiempo,
                 "lista_ordenada": lista_ordenada,
